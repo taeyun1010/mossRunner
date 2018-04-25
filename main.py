7dadd9ca991edb62828cdeb84acefcff385687bd
Taeyun Kim <taeyun1010@gmail.com>
2018-04-25 17:00:37 +0900

    returns max percentage for each file

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import subprocess
 import urllib
 from lxml import html
+from lxml import etree
 import os
 import os.path
 import re
+
+#
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+#
 
 # put the desired extension here
 extension = ".cpp"
@@ -15,10 +23,43 @@ coin2path = "C:\Users\User\Desktop\moss\coins/BTCGPU/"
 
 # get max percentage of the file happening in this page
 def getPercentage(page, coinpath):
-    indexes = [m.start() for m in re.finditer(coinpath, page)]
-    coinindex = page.find(coinpath) + len(coinpath) + 2
-    coinpercentage = page[coinindex:].split("%")[0]
-    return coinpercentage
+    maxPercentage = 0
+#     html = etree.HTML(page)
+#     table = etree.HTML(page).find("body/table")
+#     rows = iter(table)
+#     headers = [col.text for col in next(rows)]
+#     for row in rows:
+#         values = [col.text for col in row]
+#         print dict(zip(headers, values))
+    
+    
+    #indexes = [m.start() for m in re.finditer(coinpath, page)]
+
+    parsed_html = BeautifulSoup(page, "lxml")
+    table = parsed_html.body.table
+    rows = table.find_all('tr')
+    
+    # contents in the table
+    data = []
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele]) # Get rid of empty values
+    for thisdata in data:
+        if(len(thisdata) != 0):
+            thedata = thisdata[0]
+            if(coinpath in thedata):
+                print("thedata = " + thedata)
+                beginindex = page.find(coinpath) + len(coinpath) + 2
+                coinpercentage = page[beginindex:].split("%")[0]
+                thispercentage = coinpercentage
+                if(maxPercentage < thispercentage):
+                    maxPercentage = thispercentage
+    
+    #coinindex = page.find("<TR><TD><A HREF=") + len(coinpath) + 2
+    
+    #coinpercentage = page[coinindex:].split("%")[0]
+    return maxPercentage
 
 def queryServer(file1, file2):
     pipe = subprocess.Popen(["perl", "C:\Users\User\Desktop\moss\moss.pl", "-l", extensionCmd, file1, file2], stdout=subprocess.PIPE)
@@ -27,7 +68,28 @@ def queryServer(file1, file2):
         if ("http://moss.stanford.edu" in url):
             break
     #page = html.fromstring(urllib.urlopen(url).read())
+    
     page = urllib.urlopen(url).read()
+    
+    #parsedhtml2 = urllib.urlopen(url)
+    
+#     #
+#     soup = BeautifulSoup(page, 'lxml') # Parse the HTML as a string
+#     
+#     table = soup.find_all('table')[0] # Grab the first table
+#     
+#     new_table = pd.DataFrame(columns=range(0,2), index = [0]) # I know the size 
+#     
+#     row_marker = 0
+#     for row in table.find_all('tr'):
+#         column_marker = 0
+#         columns = row.find_all('td')
+#         for column in columns:
+#             new_table.iat[row_marker,column_marker] = column.get_text()
+#             column_marker += 1
+#     #
+    
+            
     #print(page)
     coin1percentage = getPercentage(page, file1)
     #coin2percentage = getPercentage(page, file2)
@@ -51,7 +113,7 @@ if __name__ == '__main__':
         for filename in [f for f in filenames if f.endswith(extension)]:
             thisfile = os.path.join(dirpath, filename)
             percentage1 = queryServer(thisfile, "C:\Users\User\Desktop\moss\coins\BTCGPU\src/*.cpp")
-            print(percentage1 + "\n")
+            print(str(percentage1) + "\n")
 #     temp = os.walk("C:\Users\User\Desktop\moss\coins/bitcoin/")
 #     print(x[0] for x in temp)
 #     
